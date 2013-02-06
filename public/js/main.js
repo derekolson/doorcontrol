2c65480d74231079f58a00abc74a969d94e7969a
Derek Olson <dolson@prophet.com>
2013-02-06 16:55:32 -0600

    initial config

--- public/js/main.js
+++ public/js/main.js
@@ -2,20 +2,7 @@ $(document).ready(init);
 
 function init() {
 	$(window).resize(onResize);
-
-}
-
-
-function openDoor(id) {
-
-	$.ajax({
-		type: "GET",
-		url: "open/" + id
-	}).done(function( msg ) {
-	
-	});
 }
 
 function onResize(){
-
 }
